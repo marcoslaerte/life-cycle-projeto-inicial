6f0fcc7450b8e4a6504955f7ad150bb81dfb89b4
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-28 20:01:40 -0300

    Adicionando elemento na lista com injeção de dependência

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -31,7 +31,23 @@ export class ListaDeCompraService {
     console.log('Instanciando dependências necessárias para o serviço.');
   }
 
-  getListaDeCompra(){
+  getListaDeCompra() {
     return this.listaDeCompra;
+  }
+
+  criarItem(nomeDoItem: string) {
+    const id = this.listaDeCompra.length + 1;
+    const item : Item = {
+      id: id,
+      nome: nomeDoItem,
+      data: new Date().toLocaleString('pt-BR'),
+      comprado: false
+    }
+    return item;
+  }
+
+  adicionarItemNaLista(nomeDoItem: string) {
+    const item = this.criarItem(nomeDoItem);
+    this.listaDeCompra.push(item);
   }
 }
