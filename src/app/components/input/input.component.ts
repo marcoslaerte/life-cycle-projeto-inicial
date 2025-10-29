23a157b0d36ca04b638d832d2bc9575a2f481531
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-29 20:30:13 -0300

    Alterando item ao invés de adicionar na lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -10,6 +10,8 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 export class InputComponent implements OnInit, OnChanges {
 
   @Input() itemQueVaiSerEditado!: Item;
+  editando = false;
+  textoBtn = 'Salvar item'
   valorItem! : string;
 
   constructor(private listaService: ListaDeCompraService) { }
@@ -25,8 +27,17 @@ export class InputComponent implements OnInit, OnChanges {
     this.valorItem = '';
   }
 
+  editarItem() {
+    this.listaService.editarItemDaLisa(this.itemQueVaiSerEditado, this.valorItem);
+    this.limparCampo();
+    this.editando = false;
+    this.textoBtn = 'Salvar item';
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     if(!changes['itemQueVaiSerEditado'].firstChange) {
+      this.editando = true;
+      this.textoBtn = 'Editar item';
       this.valorItem = this.itemQueVaiSerEditado?.nome;
     }
   }
